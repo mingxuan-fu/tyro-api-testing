6a99c14e813629c3186d3da9bfb2fccade49dd00
Mingxuan Fu <mfuu0003@student.monash.edu>
2022-04-24 20:33:19 +1000

    added documents and README.md

---
+++ README.md
@@ -0,0 +1,28 @@
+# TYRO PAYMENT Product API Testing
+
+This Python script performs minor testing of the Tyro Banking API, specifically the product API. 
+
+Python's in-built unit testing framework is chosen for this task. Though this is not strictly unit testing, the Python unit testing framework is a light and simple to use testing framework, and is easily extendable by adding more test cases. This is paired with the 'requests' library, which allows for easy http calls through python, making the code less comoplex.
+## Pre-requisites
+
+* This script does not have access to the server code and relies on public interfaces, thus the API must be online and accessible through the internet.
+
+* The computer from which the script is run must be able to access the internet.
+
+* The required libraries as specified in requirement.txt must be installed.
+
+* Python must be installed.
+
+## Assumptions
+
+* The ProductID of various products is unlikely to change, the script would have to be updated if the ProductID of 'Tyro Business Loan' changes. Though as this is an ID, it is presumed that this is unlikely.
+
+## Addtional Scenarios
+
+Both API end point used should return status 400 if any of the following condition is met
+
+* the header is missing
+
+* the header contains non-numeric 'x-v' version
+
+* the header contains a 'x-v' version less than 1
--- product_api_tests.py
+++ product_api_tests.py
@@ -1,51 +1,64 @@
-import re
-import requests as req
-import sys, json, pprint, unittest
+"""
+TYRO Product API test cases
+Author: Mingxuan Fu
+"""
 
-api_ver = 1
-url = "https://public.cdr.tyro.com/cds-au/v1/banking/products"
-business_loan_id = "b5ee1091-e3af-4517-8f8f-8cc52434472b"
+import requests as req
+import json, pprint, unittest
+
+# set consts
+API_VER = 1
+URL = "https://public.cdr.tyro.com/cds-au/v1/banking/products"
+BUSINESS_LOAN_ID = "b5ee1091-e3af-4517-8f8f-8cc52434472b"
 
 pp = pprint.PrettyPrinter()
 
-header_params = {"x-v": str(api_ver)}
-bad_header_params = {"x-v": str(-1)}
+header_params = {"x-v": str(API_VER)}
+invalid_header_params = {"x-v": str(-1)}
 
 def get_term_deposits(products):
+    """
+    Helper function to get term deposit products from list of products
+    """
     return list(filter(lambda product: 'Term Deposit' in product['name'], products))
-
 
 class TestProductApi(unittest.TestCase):
     def test_get_products(self):
-        r = req.get(url, headers=header_params)
+        r = req.get(URL, headers=header_params)
 
         self.assertEqual(r.status_code, 200)
 
+        # Get term deposit products
         products = json.loads(r.text)['data']['products']
         term_deposits = get_term_deposits(products)
 
+        # Print term deposit products to terminal
         print('\nterm deposit products\n')
         pp.pprint(term_deposits)
 
     def test_get_product_detail(self):
-        r = req.get(f'{url}/{business_loan_id}', headers=header_params)
+        r = req.get(f'{URL}/{BUSINESS_LOAN_ID}', headers=header_params)
 
         self.assertEqual(r.status_code, 200)
 
+        # Get Business loan eligibility
         data = json.loads(r.text)['data']
         eligibility = data['eligibility']
         
+        # Print eligibility to terminal
         print('\neligibility of business loan\n')
         pp.pprint(eligibility)
 
     def test_get_products_invalid_headers(self):
-        r = req.get(f'{url}/{business_loan_id}', headers=bad_header_params)
+        r = req.get(f'{URL}/{BUSINESS_LOAN_ID}', headers=bad_header_params)
 
+        # invalid header should result in status 400
         self.assertEqual(r.status_code, 400)
     
     def test_get_product_detail_missing_headers(self):
-        r = req.get(url, headers={})
+        r = req.get(URL, headers={})
 
+        # missing header x-v value should result in status 400
         self.assertEqual(r.status_code, 400)
 
 if __name__ == '__main__':
